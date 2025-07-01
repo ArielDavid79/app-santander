bd2d77df9355f42b27dc3630c1cd32a75bd2de1a
Senac Hub - Sala 203 <hubsala203@aluno.ms.senac.br>
2025-06-30 21:41:37 -0400

    Atualizando app

--- lib/controllers/views/dashboard.dart
+++ lib/controllers/views/dashboard.dart
@@ -8,64 +8,155 @@ class Dashboard extends StatefulWidget {
 }
 
 class _DashboardState extends State<Dashboard> {
+  bool cardAberto = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         children: [
-          Stack(children: [
-            Image.asset("fundo_dashboard.png"),
-            Padding(
-              padding: const EdgeInsets.only(right: 12.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Image.asset(
-                    "teste3.png",
-                    width: 150,
-                  ),
-                  
-                  Row(
+          Container(
+            height:
+                cardAberto
+                    ? MediaQuery.of(context).size.height * 0.50
+                    : MediaQuery.of(context).size.height * 0.36,
+            child: Stack(
+              children: [
+                Image.asset("fundo_dashboard.png"),
+                Padding(
+                  padding: const EdgeInsets.only(right: 12),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Icon(
-                        Icons.search_outlined,
-                        color: Colors.white,
-                      ),
-                      Icon(
-                        Icons.message_outlined,
-                        color: Colors.white,
-                      ),
-                      Icon(
-                        Icons.notifications_outlined,
-                        color: Colors.white,
+                      Image.asset("teste3.png", width: 150),
+                      Row(
+                        children: [
+                          Icon(Icons.search_outlined, color: Colors.white),
+                          Icon(Icons.message_outlined, color: Colors.white),
+                          Icon(
+                            Icons.notifications_outlined,
+                            color: Colors.white,
+                          ),
+                        ],
                       ),
                     ],
                   ),
-                  
-                ],
-                
-              ),
+                ),
+                Positioned(
+                  top: 150,
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 12),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Olá Ariel David",
+                          style: TextStyle(color: Colors.white, fontSize: 17),
+                        ),
+                        Text(
+                          "Ag 1116 Cc 154879-1",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Positioned(
+                  top: MediaQuery.of(context).size.height * 0.26,
+                  child: Padding(
+                    padding: EdgeInsets.only(left: 12, right: 12),
+                    child: Card(
+                      elevation: 10,
+                      color: Colors.white,
+                      child: Container(
+                        padding: EdgeInsets.all(12),
+                        width: MediaQuery.of(context).size.width * 0.93,
+                        height: cardAberto ? null : null,
+                        child: Column(
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Row(
+                                  children: [
+                                    Icon(
+                                      Icons.monetization_on,
+                                      color: Colors.red,
+                                    ),
+                                    SizedBox(width: 10),
+                                    Text(
+                                      "Saldo Disponivel",
+                                      style: TextStyle(fontSize: 16),
+                                    ),
+                                  ],
+                                ),
+                                IconButton(
+                                  onPressed: () {
+                                    setState(() {
+                                      cardAberto = !cardAberto;
+                                    });
+                                  },
+                                  icon:
+                                      cardAberto
+                                          ? Icon(Icons.keyboard_arrow_up)
+                                          : Icon(Icons.keyboard_arrow_down),
+                                ),
+                              ],
+                            ),
+                            cardAberto
+                                ? Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      "R\$ 1.000.000,00",
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 22,
+                                      ),
+                                    ),
+                                    Text(
+                                      "Saldo + Limite R\$ 25.000.000,00",
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 16,
+                                      ),
+                                    ),
+                                    Text(
+                                      "Entenda seu limite",
+                                      style: TextStyle(
+                                        color: Colors.red,
+                                        decoration: TextDecoration.underline,
+                                      ),
+                                    ),
+                                    Divider(),
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: [
+                                        Text(
+                                          "ver extrato",
+                                          style: TextStyle(
+                                            fontSize: 18,
+                                            color: Colors.red,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                )
+                                : Container(),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
-            Positioned(
-              top: 145,
-              child: Padding(
-                padding: const EdgeInsets.only(left: 12),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                   Text("ola,Ariel",
-                  style: TextStyle(color:Colors.white, fontSize: 17,),
-                   ),
-                   Text("ag 1578  cc 125487-5  ",
-                  style: TextStyle(
-                    color:Colors.white, fontSize: 18,fontWeight:FontWeight.bold ),
-                   ),
-                ],),
-              ),
-            )
-          ],
-          )
-
+          ),
         ],
       ),
     );
